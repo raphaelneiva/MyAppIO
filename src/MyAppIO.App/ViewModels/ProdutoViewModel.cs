d88aec114d7893cd03a1e5a0cb0a32f50e58fc55
Raphael Neiva <raphael.neiva@solutis.com.br>
2023-01-04 08:37:58 -0300

    adding style to pages again

--- src/MyAppIO.App/ViewModels/ProdutoViewModel.cs
+++ src/MyAppIO.App/ViewModels/ProdutoViewModel.cs
@@ -21,7 +21,10 @@ namespace MyAppIO.App.ViewModels
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
+
+        [DisplayName("Imagem do Produto")]
         public IFormFile ImagemUpload { get; set; }
+
         public string Imagem { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
